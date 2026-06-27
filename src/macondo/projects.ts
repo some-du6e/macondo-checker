5d6e699377b82f364c0d590a5b818a27d27b1e5f
some-du6e <103129948+some-du6e@users.noreply.github.com>
2026-06-27 15:20:51 -0600

    feat(macondo): add api

---
+++ src/macondo/projects.ts
@@ -0,0 +1,11 @@
+import {projectSchema} from "./types";
+
+async function getProject(id: number) {
+    let BASE_URL = process.env.BASE_URL || "https://macondo.hackclub.com";
+
+    let res = fetch(`${BASE_URL}/api/projects/${id}`)
+
+    
+
+
+}
